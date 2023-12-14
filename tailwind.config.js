e58c2e8630dd10b6e521f1d4893705bdf5a37a7c
Darius <dariuskumza@gmail.com>
2023-12-14 22:00:56 +0200

    purge css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: ["./**/*.html", "./**/*.js"],
+   content: ["./html/**/*.html", "./html/**/*.js"],
    theme: {
+      extend: {},
+   },
+   variants: {
       extend: {},
    },
    plugins: [],
