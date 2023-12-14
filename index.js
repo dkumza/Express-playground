eca96b398db84eb210219d5789ae9221852e9fbd
Darius <dariuskumza@gmail.com>
2023-12-14 21:28:39 +0200

    Update styles and history page

--- index.js
+++ index.js
@@ -17,10 +17,29 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
    const allData = JSON.parse(fs.readFileSync("./messages.json", "utf8"));
    allData.push(req.body);
-   console.log(allData);
    fs.writeFileSync("./messages.json", JSON.stringify(allData));
 
    res.json({ status: "OK" });
+});
+
+app.get("/history", (req, res) => {
+   const html = fs.readFileSync("./html/history.html", "utf8");
+
+   const allData = JSON.parse(fs.readFileSync("./messages.json", "utf8"));
+
+   const mappedData = allData
+      .map(
+         (msg) =>
+            `<div class="border border-sky-100 rounded p-4 w-96 shadow bg-sky-100 text-sky-500 flex flex-col">
+                <h2 class="font-semibold text-xl text-center">${msg.name}</h2>
+                <p>${msg.message}</p>
+             </div>`
+      )
+      .join("");
+
+   const finalHtml = html.replace("[[[data]]]", mappedData);
+
+   res.send(finalHtml);
 });
 
 app.listen(port, () => {
